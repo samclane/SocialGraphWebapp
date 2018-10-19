c0d080d88ba23cadca92d31e0f5eaa2b4cecebac
Sawyer McLane <samclane@gmail.com>
2018-10-19 15:20:19 -0600

    Added simple graph app

---
+++ graphs/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from pylab import *
+from io import BytesIO
+from matplotlib.figure import Figure
+
+def index(request):
+    # Construct the graph
+    fig = Figure()
+    ax = fig.add_subplot(111)
+    x = np.arange(0, 2*pi, 0.01)
+    s = cos(x)**2
+    ax.plot(x, s)
+
+    plot(x, s)
+
+    xlabel('xlabel(X)')
+    ylabel('ylabel(Y)')
+    title('Simple Graph!')
+    grid(True)
+
+    buf = BytesIO()
+    savefig(buf, format='png')
+    plt.close(fig)
+
+    response = HttpResponse(buf.getvalue(), content_type='image/png')
+    return response
