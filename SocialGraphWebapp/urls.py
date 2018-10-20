d647797fb885bdb0d49318cc42295f34ae79950c
Sawyer McLane <samclane@gmail.com>
2018-10-20 12:48:49 -0600

    Added initialization code in the appropriate place.

--- SocialGraphWebapp/urls.py
+++ SocialGraphWebapp/urls.py
@@ -16,11 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from graphs.views import init_svm_graphs
-
 urlpatterns = [
     path('graphs/', include('graphs.urls')),
     path('admin/', admin.site.urls),
 ]
 
-init_svm_graphs()
--- graphs/apps.py
+++ graphs/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
 
+from .views import init_svm_graphs
+
 
 class GraphsConfig(AppConfig):
     name = 'graphs'
+
+    def ready(self):
+        print("hello")
+        init_svm_graphs()
