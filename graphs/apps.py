0e115dae1ab357a252d3405ac460a0ac806ed31a
Sawyer McLane <samclane@gmail.com>
2018-10-20 19:39:09 -0600

    Updated to try running ready() only once. Will change heroku values after this.

--- graphs/apps.py
+++ graphs/apps.py
@@ -1,11 +1,11 @@
 from django.apps import AppConfig
 
-from .views import init_svm_graphs
+from .socialgraphing import init_svm_graphs
 
 
 class GraphsConfig(AppConfig):
     name = 'graphs'
 
     def ready(self):
-        print("hello")
+        print("Initializing app...")
         init_svm_graphs()
