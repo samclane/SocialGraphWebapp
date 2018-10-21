802970ea8649e8ff5a42d886c4c6f557ffa68447
Sawyer McLane <samclane@gmail.com>
2018-10-20 21:22:29 -0600

    Now serves an HTML document instead of just an image. Uses nice templates and is generally more Django-ish.

--- SocialGraphWebapp/urls.py
+++ SocialGraphWebapp/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('graphs/', include('graphs.urls')),
+    path('', include('graphs.urls')),
     path('admin/', admin.site.urls),
 ]
 
--- graphs/views.py
+++ graphs/views.py
@@ -1,12 +1,18 @@
 from io import BytesIO
 
 from django.http import HttpResponse
+from django.template import loader
 from pylab import *
 
 
-def index(request):
+def graph(request):
     buf = BytesIO()
     savefig(buf, format='png')
-
     response = HttpResponse(buf.getvalue(), content_type='image/png')
     return response
+
+
+def index(request):
+    template = loader.get_template('graphs/index.html')
+    return HttpResponse(template.render(request=request))
+
