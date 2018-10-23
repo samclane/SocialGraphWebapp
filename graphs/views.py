0f26068ff5c2209a188aaab6d4f14e9ddee7aa02
Sawyer McLane <samclane@gmail.com>
2018-10-22 18:50:50 -0600

    Updated to use mpld3.

--- graphs/views.py
+++ graphs/views.py
@@ -1,18 +1,81 @@
-from io import BytesIO
+import json
+import random
 
+import numpy
 from django.http import HttpResponse
 from django.template import loader
-from pylab import *
+from mpld3 import urls
+from mpld3._display import TEMPLATE_DICT
+from mpld3.mpld3renderer import MPLD3Renderer
+from mpld3.mplexporter import Exporter
+from mpld3.utils import get_id
+from pylab import re, gcf
+
+
+# Had to rewrite some lines to let ndarrays through from mpld3._display.py
+class NumpyEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
+
+    def default(self, obj):
+        if isinstance(obj, (numpy.int_, numpy.intc, numpy.intp, numpy.int8,
+                            numpy.int16, numpy.int32, numpy.int64, numpy.uint8,
+                            numpy.uint16, numpy.uint32, numpy.uint64)):
+            return int(obj)
+        elif isinstance(obj, (numpy.float_, numpy.float16, numpy.float32,
+                              numpy.float64)):
+            return float(obj)
+        elif isinstance(obj, (numpy.ndarray,)):  #### This is the fix
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+
+def fig_to_html(fig, d3_url=None, mpld3_url=None, no_extras=False,
+                template_type="general", figid=None, use_http=False, **kwargs):
+    template = TEMPLATE_DICT[template_type]
+
+    # TODO: allow fig to be a list of figures?
+    d3_url = d3_url or urls.D3_URL
+    mpld3_url = mpld3_url or urls.MPLD3_URL
+
+    if use_http:
+        d3_url = d3_url.replace('https://', 'http://')
+        mpld3_url = mpld3_url.replace('https://', 'http://')
+
+    if figid is None:
+        figid = 'fig_' + get_id(fig) + str(int(random.random() * 1E10))
+    elif re.search('\s', figid):
+        raise ValueError("figid must not contain spaces")
+
+    renderer = MPLD3Renderer()
+    Exporter(renderer, close_mpl=False, **kwargs).run(fig)
+
+    fig, figure_json, extra_css, extra_js = renderer.finished_figures[0]
+
+    if no_extras:
+        extra_css = ""
+        extra_js = ""
+
+    return template.render(figid=json.dumps(figid),
+                           d3_url=d3_url,
+                           mpld3_url=mpld3_url,
+                           figure_json=json.dumps(figure_json, cls=NumpyEncoder),
+                           extra_css=extra_css,
+                           extra_js=extra_js)
+# END UGH
 
 
 def graphs(request):
+    """
     buf = BytesIO()
     savefig(buf, format='png')
     response = HttpResponse(buf.getvalue(), content_type='image/png')
     return response
+    """
+    fig = gcf()
+    html = fig_to_html(fig)
+    return HttpResponse(html)
 
 
 def index(request):
     template = loader.get_template('graphs/index.html')
     return HttpResponse(template.render(request=request))
-
